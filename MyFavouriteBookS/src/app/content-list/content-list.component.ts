dec8dfbe1bd53d8faaf21d0017cc7099b880a520
ManpreetKaur1101 <122822465+ManpreetKaur1101@users.noreply.github.com>
2024-02-09 20:08:20 -0500

    Assignment 3

--- MyFavouriteBookS/src/app/content-list/content-list.component.ts
+++ MyFavouriteBookS/src/app/content-list/content-list.component.ts
@@ -11,18 +11,16 @@ export class ContentListComponent {
 
   name : string | undefined;
    contentArray: Content[] = [];
-   logContentInfo(id: string, title: string) {
-    console.log("Content ID:", id);
-    console.log("Content Title:", title);
-  }
   
-  
+   typePresent:boolean | undefined;
+  inputValue:string='';
+
   constructor(){
   this.contentArray.push(
     
       { 
         id: 1, 
-        title: 'Content 1', 
+        title: 'Wrong Place Wrong Time ', 
         description: 'Description for Content 1', 
         creator: 'Creator 1', 
         imgURL: 'https://i.guim.co.uk/img/media/a39b7a5a36961a7f4ba5ec245c4a20c43f087c84/0_0_226_346/master/226.jpg?width=300&quality=45&auto=format&fit=max&dpr=2&s=87a8f9e92fb6146f7e32a0ebd9f386da', 
@@ -31,20 +29,20 @@ export class ContentListComponent {
       },
       { 
         id: 2, 
-        title: 'Content 2', 
+        title: 'What Does not kill You', 
         description: 'Description for Content 2', 
         creator: 'Creator 2', 
         imgURL: 'https://www.refinery29.com/images/10229895.jpg', 
-        type: 'Type 2', 
+        type: '', 
         tags: [] 
       },
       { 
         id: 3, 
-        title: 'Content 3', 
+        title: 'It Ends With Us', 
         description: 'Description for Content 3', 
         creator: 'Creator 3', 
         imgURL: 'https://product-images-cdn.liketoknow.it/n55_eSfsWH0uGQh0KSVRN7Oorh3IxTG9aepSQjBUC75ZzmGxJNUdLBUdAuGP5KhMZZ2g4UNM5xmQ3qHmUuydfqLp4cOFdGiDf_LfMrLQ3WGRkBbmn7aUPCgoXOu0nR1HcRmXUmQH.1w1Umvt5AN_h9nFllKaxXPTTauY85S2LWfDmWME..ff4opw9Jk-', 
-        type: 'Type 3', 
+        type: '', 
         tags: ['Tag5', 'Tag6'] 
       },
       { 
@@ -53,39 +51,71 @@ export class ContentListComponent {
         description: 'Description for Content 3', 
         creator: 'Creator 3', 
         imgURL: 'https://rathertoofondofbooks.files.wordpress.com/2019/10/39940912._sy475_-1.jpg?w=630', 
-        type: 'Type 3', 
+        type: 'Thrill', 
         tags: ['Tag5', 'Tag6'] 
       },  { 
         id: 5, 
-        title: 'Content 5', 
+        title: 'Harry Potter', 
         description: 'Description for Content 3', 
         creator: 'Creator 3', 
         imgURL: 'https://hips.hearstapps.com/hmg-prod/images/harry-potter-and-the-philosopher-s-stone-1615905532.jpg?crop=1xw:1xh;center,top&resize=980:*', 
-        type: 'Type 3', 
+        type: 'Thrill', 
         tags: ['Tag5', 'Tag6'] 
       },
       { 
         id: 6, 
-        title: 'Content 6', 
+        title: 'Tonight I Burn', 
         description: 'Description for Content 3', 
         creator: 'Creator 3', 
-        imgURL: 'https://example.com/image3.jpg', 
-        type: 'Type 3', 
+        imgURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlUqhuif5PpUflqVeF5juTbVDgdpr-kyKOVdlx_uHYmZpFP44VAf2HIOlnmxO_kPG-7Ow&usqp=CAU', 
+        type: 'Thrill', 
+        tags: ['Tag5', 'Tag6'] ,
+      },
+      { 
+        id: 7, 
+        title: 'In THw Heart Of Sea', 
+        description: 'Description for Content 7', 
+        creator: 'Creator 3', 
+        imgURL: 'https://images3.penguinrandomhouse.com/cover/9780141001821', 
+        type: 'Romance', 
         tags: ['Tag5', 'Tag6'] 
-      }
+      },
+     {
+       id: 8, 
+      title: 'The Book Of LOst Names', 
+      description: 'Description for Content 7', 
+      creator: 'Creator 3', 
+      imgURL: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1573742414l/52762903.jpg', 
+      type: 'Romance', 
+      tags: ['Tag5', 'Tag6'] 
+    },
+    {
+    id: 9, 
+    title: 'Stephen King ', 
+    description: 'Description for Content 7', 
+    creator: 'Creator 3', 
+    imgURL: 'https://miblart.com/wp-content/uploads/2023/04/9781529355413-667x1024.jpg', 
+    type: 'Romance', 
+    tags: ['Tag5', 'Tag6'] 
+  }
+
      );
-    
+// Add more content items if needed
+    }
 
-   
-    
-
-
-
-
-    
-    
-    
-      // Add more content items if needed
+    search(str:string){
+     
+      for(let  i=0; i < this.contentArray.length; i++){
+        if(this.contentArray[i].title==str)
+        {
+          this.typePresent=true;
+          break;
+        }
+      
+      else if(this.contentArray[i].title!=str)
+       this.typePresent=false;
+      }
+      
     }
 
 }
